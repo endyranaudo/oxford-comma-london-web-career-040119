07f907870e9030bc1f5dd510e3f7f376e178a94e
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-24 12:12:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    last_element = ", and #{array.pop}"
+    last_element = ", and #{array[-1]}"
     array.pop
     binding.pry
   end
